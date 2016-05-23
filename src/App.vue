f37b810ccd5a488e93d85c1b01c5b181722d97f3
Ra100 <ra100x@gmail.com>
2016-05-23 12:09:43 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<style lang="stylus" src="./styles/App.styl"></style>
+
+<template lang="pug" src="./templates/App.pug"></template>
+
+<script>
+  import Hello from './components/Hello.vue'
+  export default {
+    components: {
+      Hello
+    }
+  }
+</script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,14 @@
+<template lang="pug" src="./templates/Hello.pug"></template>
+
+<script>
+export default {
+  data () {
+    return {
+      // Note: modifying `msg` below will not cause changes to occur with
+      // hot-reload. As reloaded components preserve their initial state,
+      // modifying these values will have no effect.
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
