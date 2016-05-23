62b413dabe7e58b9e69593f83215572260fd0332
Ra100 <ra100x@gmail.com>
2016-05-23 16:23:10 +0200

    Socket loading data on created()

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,90 @@
-<style lang="stylus" src="./styles/App.styl"></style>
-
+<style lang="scss" src="./styles/App.scss"></style>
 <template lang="pug" src="./templates/App.pug"></template>
-
 <script>
-  import Hello from './components/Hello.vue'
-  export default {
-    components: {
-      Hello
+import Hello from './components/Hello.vue'
+import Vue from 'vue'
+import socketIOClient from 'socket.io-client'
+import sailsIOClient from 'sails.io.js'
+
+const io = sailsIOClient(socketIOClient)
+io.sails.url = 'https://shoutbox.rozhlas.cz'
+const socket = io.socket
+
+socket.on('connect', () => {
+  console.log('Socket connected')
+  socket.get('/csrfToken', (data) => {
+    io.sails.headers = {
+      'x-csrf-token': data._csrf
+    }
+  })
+})
+
+var App = Vue.component('app', {
+  state: {
+    name: '',
+    id: '0',
+    form: {},
+    messages: [],
+    page: 0
+  },
+  components: {
+    Hello
+  },
+  created() {
+    this.init()
+  },
+  methods: {
+    init() {
+      this.getName()
+      this.getStream()
+        .then(this.processStream)
+        .then(this.getMessages)
+        .then(this.processMessages)
+        .catch(console.error)
+    },
+    getName() {
+      this.name = document
+        .getElementById('shoutbox')
+        .getAttribute('data-shoutbox-name')
+    },
+    getStream() {
+      return new Promise((resolve, reject) => {
+        socket.get('/streams/public', {
+          name: this.name
+        }, resolve)
+      })
+    },
+    processStream(data, err) {
+      return new Promise((resolve, reject) => {
+        if (err) {
+          return reject()
+        }
+        this.id = data.id
+        this.form = data.form
+        console.log(this.id)
+        resolve()
+      })
+    },
+    getMessages() {
+      return new Promise((resolve, reject) => {
+        socket.get('/streams/messages', {
+          id: this.id,
+          skip: this.page
+        }, resolve)
+      })
+    },
+    processMessages(data, err) {
+      return new Promise((resolve, reject) => {
+        if (err) {
+          return reject(err)
+        }
+        this.messages = data
+        console.log(this.messages)
+        resolve()
+      })
     }
   }
+})
+
+export default App
 </script>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import io from 'socket.io-client'
-
-const socket = io('https://shoutbox.rozhlas.cz')
-let name = document.getElementById('shoutbox').getAttribute('data-shoutbox-name')
-
-socket.on('connect', (event) => {
-  console.log('Socket connected')
-})
-
-console.log(name)
 
 /* eslint-disable no-new */
-var app = new Vue({el: '#shoutbox', components: {App}})
-
-console.log(app)
+new Vue({
+  el: '#shoutbox',
+  components: {App}
+})
