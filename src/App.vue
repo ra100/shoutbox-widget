ce30983ef6f190da27024d7229f0f1000603bd32
Ra100 <ra100x@gmail.com>
2016-06-08 16:52:50 +0200

    Listen to push messages, update stream

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@ import ShMessage from './components/ShMessage.vue'
 import Vue from 'vue'
 import socketIOClient from 'socket.io-client'
 import sailsIOClient from 'sails.io.js'
+import _ from 'lodash'
+
+const PER_PAGE = 10
 
 const io = sailsIOClient(socketIOClient)
 io.sails.url = 'https://shoutbox.rozhlas.cz'
@@ -18,14 +21,11 @@ socket.on('connect', () => {
   })
 })
 
+function sortByDate(a, b) {
+  return (new Date(a.created) < new Date(b.created))
+}
+
 var App = Vue.component('app', {
-  state: {
-    name: '',
-    id: '0',
-    form: {},
-    messages: [],
-    page: 0
-  },
   components: {
     ShMessage
   },
@@ -35,14 +35,26 @@ var App = Vue.component('app', {
       id: this.id,
       form: this.form,
       messages: this.messages,
+      newmessages: this.newmessages,
       page: this.page
+    }
+  },
+  computed() {
+    return {
+      new_count: function() {
+        console.log('kikotina')
+        return this.newmessages.length
+      }
     }
   },
   created() {
     this.init()
+    socket.on('stream', this.processEvent)
   },
   methods: {
     init() {
+      this.newmessages = []
+      this.page = 0
       this.getName()
       this.getStream()
         .then(this.processStream)
@@ -88,6 +100,51 @@ var App = Vue.component('app', {
         this.messages = data
         resolve()
       })
+    },
+    processEvent(event) {
+      switch (event.verb) {
+        case 'addedTo':
+          let message = event.added
+          if (!message.published) {
+            this.removeMessage(message.id)
+            return
+          } else {
+            this.addMessage(message)
+          }
+          break
+        case 'removedFrom':
+          this.removeMessage(event.removedId)
+          break
+      }
+    },
+    addMessage(message) {
+      let m = this.messages
+      for (let i in m) {
+        if (m[i].id === message.id) {
+          m[i] = message
+          return
+        }
+      }
+      let n = this.newmessages
+      if (new Date(message.created) > new Date(m[m.length - 1].created)) {
+        n.push(message)
+        n.sort(sortByDate)
+        n = _.slice(n, 0, PER_PAGE)
+      }
+    },
+    removeMessage(id) {
+      this.messages = this.messages.filter(function(e) {
+        return (e.id !== id)
+      })
+      this.newmessages = this.newmessages.filter(function(e) {
+        return (e.id !== id)
+      })
+    },
+    mergeMessages() {
+      let m = _.unionBy(this.messages, this.newmessages, 'id')
+      m.sort(sortByDate)
+      this.newmessages = []
+      this.messages = _.slice(m, 0, PER_PAGE)
     }
   }
 })
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMoment from 'vue-moment'
+
+Vue.use(VueMoment)
 
 /* eslint-disable no-new */
 new Vue({
