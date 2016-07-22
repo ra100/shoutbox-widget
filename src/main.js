53af4566f60e259d1780ec610578aaa6ffa3d211
Ra100 <ra100x@gmail.com>
2016-07-22 14:41:33 +0200

    WIP validation

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueValidator from 'vue-validator'
 
 /* eslint-disable no-new */
+Vue.use(VueValidator)
 new Vue({
   el: '#shoutbox',
   components: {App}
