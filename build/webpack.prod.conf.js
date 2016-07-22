d323a883bc82af6d5e0a12ad504f537b7e0b8a6d
Ra100 <ra100x@gmail.com>
2016-07-22 13:08:25 +0200

    Use package version in build script
    Don't use chunkhash and contenthash

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -9,6 +9,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
   : config.build.env
+var pkg = require('../package.json')
 
 module.exports = merge(baseWebpackConfig, {
   module: {
@@ -17,8 +18,8 @@ module.exports = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? '#source-map' : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    filename: utils.assetsPath('js/[name].' + pkg.version + '.js'),
+    chunkFilename: utils.assetsPath('js/[id].' + pkg.version + '.js')
   },
   vue: {
     loaders: utils.cssLoaders({
@@ -38,7 +39,7 @@ module.exports = merge(baseWebpackConfig, {
     }),
     new webpack.optimize.OccurenceOrderPlugin(),
     // extract css into its own file
-    new ExtractTextPlugin(utils.assetsPath('css/[name].[contenthash].css')),
+    new ExtractTextPlugin(utils.assetsPath('css/[name].' + pkg.version + '.css')),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
     // see https://github.com/ampedandwired/html-webpack-plugin
